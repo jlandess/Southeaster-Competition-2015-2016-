801a72197f5156f1bd0c7429523a2d49461c1b48
James Landess <JLandess@Jamess-MacBook-Pro.local>
2015-10-07 23:34:53 -0400

    Initial Commit

---
+++ Southeaster-Competition-2015-2016/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Southeaster-Competition-2015-2016
+//
+//  Created by James Landess on 10/7/15.
+//  Copyright (c) 2015 James Landess. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
